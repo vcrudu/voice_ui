7a0b783f84da57042b7c43befc5b2cd24de4f502
victor.crudu-at-160466482332 <vcrudu@hotmail.com>
2019-06-15 22:52:19 +0100

    Fixed audio issues

--- app/constants/appSettings.js
+++ app/constants/appSettings.js
@@ -4,12 +4,12 @@ const host = 'http://localhost';
 //const host = 'https://app.trichromehealth.com';
 const port = '8081';
 const APP_SETTINGS = {
-    //serverUrl:"https://app.trichromehealth.com/",
-    //serverApiUrl:"https://app.trichromehealth.com/v1/api/",
+    serverUrl:"https://app.trichromehealth.com/",
+    serverApiUrl:"https://app.trichromehealth.com/v1/api/",
     enrolServerUrl: "http://52.50.25.185/",
     //enrolServerUrl: "http://localhost:8080/",
-    serverUrl: `${host}:${port}/`,
-    serverApiUrl:`${host}:${port}/v1/api/`,
+    //serverUrl: `${host}:${port}/`,
+    //serverApiUrl:`${host}:${port}/v1/api/`,
     stateKey: "STATE"
 }
 
